b10d80a046e7da1ae2647c7b25295cc42a1a6426
Samuel Aguiar <samuelaguiarp@gmail.com>
2020-02-19 16:42:55 -0300

    Added Topbar Changed home-assets to assets

---
+++ assets/js/topbar.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body");
+const topbar = document.createElement("div");
+topbar.id = "topbar";
+topbar.innerHTML = `
+  <div class="back-arrow">
+    <a href="/"><i class="fas fa-arrow-left"></i>back</a>
+  </div>
+  <div class="links">
+    <a href="mailto:samuelaguiarp@gmail.com"><i class="fas fa-envelope"></i></a>
+    <a href="https://www.linkedin.com/in/samuelaguiar/" target="_blank"><i class="fab fa-linkedin"></i></a>
+    <a href="https://github.com/samuelaguiar/" target="_blank"><i class="fab fa-github"></i></a>
+  </div>
+`;
+body.insertAdjacentElement("afterbegin", topbar);
